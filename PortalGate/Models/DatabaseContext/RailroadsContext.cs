993e0a19250faf81c59a6274f4e0ff33f1d8f1c7
CodeDrummer <code.drummer87@gmail.com>
2021-02-09 00:06:23 +0600

    Railroads database has been created

---
+++ PortalGate/Models/DatabaseContext/RailroadsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalGate.Models.DatabaseContext
+{
+    public class RailroadsContext : DbContext
+    {
+        public DbSet<Railroad> Railroads { get; set; }
+
+        public RailroadsContext(DbContextOptions<RailroadsContext> options) : base(options)
+        { }
+    }
+}
---
+++ PortalGate/Models/Railroad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalGate.Models
+{
+    public class Railroad
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullTitle { get; set; }
+        public string Abbreviation { get; set; }
+        public string Code { get; set; }
+    }
+}
