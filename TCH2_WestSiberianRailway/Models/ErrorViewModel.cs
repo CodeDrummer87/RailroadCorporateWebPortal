c0f2bbf46a430b8ed329e67129af93b35fa05212
CodeDrummer <code.drummer87@gmail.com>
2021-02-18 03:50:18 +0600

    An empty project for the TChE-2 unit of the West Siberian Railway has been created

---
+++ TCH2_WestSiberianRailway/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TCH2_WestSiberianRailway.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
