41085b9774dea541b29a966f418a7d8ba110e978
CodeDrummer <code.drummer87@gmail.com>
2021-02-22 01:36:25 +0600

    RailwayPortalClassLibrary has been created. EntityFrameworkCoreSqlServer has been added.

---
+++ RailwayPortalClassLibrary/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RailwayPortalClassLibrary
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+
+        public Position()
+        {
+            Users = new List<User>();
+        }
+    }
+}
---
+++ TCH2_WestSiberianRailway/Models/DatabaseContext/TCH2_WSRailwayContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RailwayPortalClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TCH2_WestSiberianRailway.Models.DatabaseContext
+{
+    public class TCH2_WSRailwayContext : DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public TCH2_WSRailwayContext(DbContextOptions<TCH2_WSRailwayContext> options) : base(options)
+        {}
+    }
+}
