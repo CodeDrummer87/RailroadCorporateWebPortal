aa2a7333a906fd682ddf36f7e8ffd6f0ea74f326
CodeDrummer <code.drummer87@gmail.com>
2021-02-22 00:36:23 +0600

    Sign in form has been added

--- TCH2_WestSiberianRailway/wwwroot/js/site.js
+++ TCH2_WestSiberianRailway/wwwroot/js/site.js
@@ -1,1 +1,27 @@
-﻿
+﻿$(document).ready(function () {
+
+	$('#inpSignIn').focus();
+
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            // Temporary code
+            DisplayCurrentMessage('Клавиша Enter была нажата', true);
+        }
+    });
+
+});
+
+function DisplayCurrentMessage(message, success) {
+    if (success) {
+        $('#currentMessage').css('color', 'green').text(message);
+    }
+    else {
+        $('#currentMessage').css('color', 'red').text(message);
+    }
+
+    setTimeout(ClearCurrentMessage, 2500);
+}
+
+function ClearCurrentMessage() {
+    $('#currentMessage').text('');
+}
